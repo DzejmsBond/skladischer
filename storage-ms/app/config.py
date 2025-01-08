92be2b599fe4f44d02d6965847258f0c97e6d464
GitCene <juremz.cene@gmail.com>
2025-01-08 12:43:38 +0100

    Pass environment variables in the proper way.

--- storage-ms/app/config.py
+++ storage-ms/app/config.py
@@ -7,8 +7,9 @@ import os
 
 # TODO: This should not be in the production version?
 #       Is this the best way to do this?
-ENV_PATH = Path(__file__).resolve().parent.parent / 'config.env'
-load_dotenv(dotenv_path=ENV_PATH)
+# Commented out to pass environment variables in the proper way
+#ENV_PATH = Path(__file__).resolve().parent.parent / 'config.env'
+#load_dotenv(dotenv_path=ENV_PATH)
 
 # Access variables.
 MONGO_URL = os.getenv("DATABASE_URL")
