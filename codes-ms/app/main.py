fd9c6b32f31d7df7dbb1cc031b4cd8d0a5a50946
GitCene <juremz.cene@gmail.com>
2025-01-09 19:46:10 +0100

    Remapped more ports and code-ms misconfigged map

--- codes-ms/app/main.py
+++ codes-ms/app/main.py
@@ -17,4 +17,4 @@ app = FastAPI(
 app.include_router(code_api.router)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8002)
+    uvicorn.run(app, host="0.0.0.0", port=8002)
