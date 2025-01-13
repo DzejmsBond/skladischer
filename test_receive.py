4a986bbb8f412f7a0551707edc9850606a53fd6e
Edelwy <nina.mislej1@gmail.com>
2025-01-13 06:29:22 +0100

    implement basic test that is to be moved to the storage ms

--- test_receive.py
+++ test_receive.py
@@ -4,16 +4,17 @@ import pika, sys, os, json
 
 def main():
     def callback(ch, method, properties, body):
-        data = json.loads(body)
-        print(f"Stored: {data}")
+        print(f"Data: {body}")
 
     connection = pika.BlockingConnection(pika.ConnectionParameters("localhost"))
     channel = connection.channel()
-    channel.exchange_declare(exchange="sensor-data-exchange", exchange_type="fanout")
-    result = channel.queue_declare(queue="", exclusive=True)
-    queue_name = result.method.queue
-    channel.queue_bind(exchange="sensor-data-exchange", queue=queue_name)
-    channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
+    channel.exchange_declare(exchange="sensor-data-exchange", exchange_type="topic")
+
+    # If we wish to disgard the queue as soon as the connection is closed use:
+    # CODE: Exclusive=True
+    result = channel.queue_declare(queue="ananovak", arguments={"x-max-length": 2})
+    channel.queue_bind(exchange="sensor-data-exchange", queue="ananovak")
+    channel.basic_consume(queue="ananovak", on_message_callback=callback, auto_ack=True)
     channel.start_consuming()
 
 if __name__ == '__main__':
--- test_send.py
+++ test_send.py
@@ -8,12 +8,13 @@ from fastapi import FastAPI, HTTPException
 app = FastAPI()
 
 # RabbitMQ connection setup
-connection = pika.BlockingConnection(pika.ConnectionParameters("localhost"))
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
-channel.exchange_declare(exchange="sensor-data-exchange", exchange_type="fanout")
+channel.exchange_declare(exchange="sensor-data-exchange", exchange_type="topic")
+
 channel.basic_publish(
-        exchange="sensor-data-exchange",
-        routing_key="",
-        body=json.dumps({"detail": "test message"})
-    )
+    exchange="sensor-data-exchange",
+    routing_key="ananovak",
+    body="Hello World!"
+)
 connection.close()
